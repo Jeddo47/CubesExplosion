e393b4814b1fd8ef58a44fff430f796a84a0de94
Jeddo47 <pavelprytkov47@gmail.com>
2024-05-24 15:56:19 +0300

    Edits 1

--- Assets/Scripts/CubeMultiplier.cs
+++ Assets/Scripts/CubeMultiplier.cs
@@ -4,7 +4,14 @@ public class CubeMultiplier : MonoBehaviour
 {
     [SerializeField] private CameraRaycast _cameraRaycast;
     [SerializeField] private UserUtils _userUtils;
+    [SerializeField] private Rigidbody _cube;
     [SerializeField] private float _multiplySuccessChance = 100;
+    [SerializeField] private float _minClones = 2;
+    [SerializeField] private float _maxClones = 7;
+    [SerializeField] private float _scaleDenominator = 2;
+    [SerializeField] private float _minPercent = 0;
+    [SerializeField] private float _maxPercent = 100;
+    [SerializeField] private float _multiplyChanceDenominator = 2;
 
     private void Update()
     {
@@ -23,43 +30,36 @@ public class CubeMultiplier : MonoBehaviour
     }
 
     private void MultiplyCube()
-    {
-        float minClones = 2;
-        float maxClones = 7;
-        float scaleDenominator = 2;
-        float minPercent = 0;
-        float maxPercent = 100;
-        float multiplyChanceDenominator = 2;
-
-        if (_userUtils.GetRandomNumber(minPercent, maxPercent) <= _multiplySuccessChance)
+    {        
+        if (_userUtils.GetRandomNumber(_minPercent, _maxPercent) <= _multiplySuccessChance)
         {
-            transform.localScale /= scaleDenominator;
-            _multiplySuccessChance /= multiplyChanceDenominator;
+            transform.localScale /= _scaleDenominator;
+            _multiplySuccessChance /= _multiplyChanceDenominator;
 
-            float numberOfClones = _userUtils.GetRandomNumber(minClones, maxClones);
+            float numberOfClones = _userUtils.GetRandomNumber(_minClones, _maxClones);
 
             for (int i = 0; i < numberOfClones; i++)
             {                
-                GameObject newCube = Instantiate(gameObject);
+                Rigidbody newCube = Instantiate(_cube);
                 PaintCube(newCube);
-                Explode(newCube);                
+                Explode(newCube);
             }
         }
 
         Destroy(gameObject);
     }
 
-    private void PaintCube(GameObject newCube)
+    private void PaintCube(Rigidbody newCube)
     {
         newCube.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
     }
 
-    private void Explode(GameObject newCube) 
+    private void Explode(Rigidbody newCube)
     {
         float explosionForce = 10;
         float explosionRadius = 5;
         float upwardsModifier = 1;
 
-        newCube.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, newCube.transform.position, explosionRadius, upwardsModifier, ForceMode.Impulse);
+        newCube.AddExplosionForce(explosionForce, newCube.transform.position, explosionRadius, upwardsModifier, ForceMode.Impulse);
     }
 }
